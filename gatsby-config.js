1bb75df80a3760bb675e5aea81ddaafe33c13df2
Pere Solà Claver <pere.solaclaver@gmail.com>
2021-11-15 22:19:58 +0000

    pwa stuff

--- gatsby-config.js
+++ gatsby-config.js
@@ -29,5 +29,23 @@ module.exports = {
       },
       __key: "images",
     },
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: "Award Book Subscription",
+        short_name: "Book Subscription",
+        start_url: "/",
+        background_color: "#ffffff",
+        theme_color: "#ffffff",
+        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+        // see https://developers.google.com/web/fundamentals/web-app-manifest/#display
+        display: "standalone",
+        icon: "src/images/icon.png", // This path is relative to the root of the site.
+        // An optional attribute which provides support for CORS check.
+        // If you do not provide a crossOrigin option, it will skip CORS for manifest.
+        // Any invalid keyword or empty string defaults to `anonymous`
+        croFssOrigin: `use-credentials`,
+      },
+    },
   ],
 };
